cf6d5d1e06d8a09bc4e8704bde6887fba5cffd7d
Rajansky51 <rajansky51@gmail.com>
2022-05-20 12:29:05 +0530

    new code

---
+++ org/test/Client.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Client {
+	public static void main(String[]args) {
+		int a=10;
+		if (a==10) {
+			System.out.println("a is 10");
+			
+			
+		}
+		int b=10;
+		if (b==20) {
+			System.out.println("b is 20");
+		}
+				
+	}
+
+}
